f8edaacdb8724eec317b6784b53b1fe2c8cb5e30
Unkown496 <kroloeden@gmail.com>
2026-06-27 01:21:56 +0300

    feat: add utils for express by server

---
+++ server/src/types/express.types.ts
@@ -0,0 +1,25 @@
+import type { Request } from 'express';
+
+import z from 'utils/zod.utils';
+import type { ZodType } from 'zod';
+
+export type RequestSchema<
+  Body extends ZodType,
+  Params extends ZodType,
+  Query extends ZodType,
+> = {
+  body?: Body;
+  params?: Params;
+  query?: Query;
+};
+
+export type RequestWithValidation<
+  Body extends ZodType = ZodType,
+  Params extends ZodType = ZodType,
+  Query extends ZodType = ZodType,
+> = Request<z.infer<Params>, any, z.infer<Body>, z.infer<Query>> &
+  Partial<{
+    validatedBody: z.infer<Body>;
+    validatedParams: z.infer<Params>;
+    validatedQuery: z.infer<Query>;
+  }>;
---
+++ server/src/utils/express.utils.ts
@@ -0,0 +1,58 @@
+import type { NextFunction, Request, Response } from 'express';
+import { RequestSchema, RequestWithValidation } from 'types/express.types';
+import { ZodAny, ZodError, ZodType } from 'zod';
+import z from 'utils/zod.utils';
+
+type ZodInfer<T extends ZodType> = z.infer<T>;
+
+export const validateRequest = <
+  Body extends ZodType = ZodAny,
+  Params extends ZodType = ZodAny,
+  Query extends ZodType = ZodAny,
+>(
+  schema: RequestSchema<Body, Params, Query>,
+) => {
+  return async (
+    req: RequestWithValidation<Body, Params, Query>,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const {
+        body: bodySchema,
+        query: querySchema,
+        params: paramsSchema,
+      } = schema ?? {};
+
+      if (bodySchema) req.validatedBody = await bodySchema.parseAsync(req.body);
+      if (querySchema)
+        req.validatedQuery = await querySchema.parseAsync(req.query);
+      if (paramsSchema)
+        req.validatedParams = await paramsSchema.parseAsync(req.params);
+
+      return next();
+    } catch (err) {
+      console.log(err);
+
+      if (err instanceof ZodError)
+        // @ts-ignore
+        return res.error(z.treeifyError(err as ZodError<any>));
+
+      res.internalServer('Validator Error');
+
+      return next(err);
+    }
+  };
+};
+
+export const pagination = (
+  total: number,
+  currentPage: number,
+  limit: number,
+) => {
+  const totalPages = Math.ceil(total / limit),
+    prevPage = currentPage > 1 ? currentPage - 1 : null,
+    nextPage = currentPage < totalPages ? currentPage + 1 : null;
+
+  return { total, totalPages, prevPage, nextPage };
+};
